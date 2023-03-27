b8880d0f7bf71ac4e4aec5a702b77019374aa54a
Abdullah khan <khansaffy1@gmail.com>
2023-03-28 01:29:47 +0500

    update consistence status formate

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,21 +12,23 @@ const setUser = async (req, res) => {
   const { name, email, password, verifyPassword } = req.body;
 
   if (!name || !email || !password || !verifyPassword) {
-    return res.send({ status: 'failed', message: 'Please fill the fields' });
+    return res
+      .status(400)
+      .json({ status: 'failed', msg: 'Please fill the fields' });
   }
 
   // Check if user exists
   const userExists = await User.findOne({ email });
   if (userExists)
-    return res.status(400).json({ message: 'User already exist' });
+    return res.status(400).json({ msg: 'This email is already exist!' });
 
   // check password and verify password
   if (password !== verifyPassword)
-    return res.status(400).json({ message: "Password don't match" });
+    return res.status(400).json({ msg: "Password don't match" });
 
   const newUser = await registerUser(req.body);
   await newUser.save();
-  res.status(200).send({ status: 'success', data: newUser });
+  res.status(200).json({ status: 'success', data: newUser });
 };
 
 // Get user
@@ -34,15 +36,14 @@ const getUser = async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.send({
-      status: false,
-      message: 'Please enter your email and password',
+    return res.status(400).json({
+      msg: 'Please enter your email and password',
     });
   }
 
   try {
     const user = await loginUser(req.body);
-    if (!user) return res.status(404).json({ message: "User doesn't exist" });
+    if (!user) return res.status(404).json({ msg: "User doesn't exist" });
 
     const hashedPassword = CryptoJS.AES.decrypt(
       user.password,
@@ -52,7 +53,7 @@ const getUser = async (req, res) => {
     const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
     if (OriginalPassword !== password) {
-      return res.status(401).json({ msg: 'wrong password credentials' });
+      return res.status(400).json({ msg: 'wrong password credentials' });
     }
 
     const accessToken = jwt.sign(
